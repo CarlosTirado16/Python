8baaeb80116451af2ae7faa3942195a5694f6ddd
CarlosTirado16 <carlos.tirado.b@icloud.com>
2025-02-07 10:40:54 -0600

    Corrigiendo probandogit.py

--- probandogit.py
+++ probandogit.py
@@ -1,2 +1,2 @@
-"Hola estoy probando que esto se haya sincronizadoa github"
-"Ya está todo correcto"
+print("Hola estoy probando que esto se haya sincronizadoa github")
+print("Ya está todo correcto")
