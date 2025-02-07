1fc8c1cf70038ee84cc74a3871ec38185ef17392
CarlosTirado16 <carlos.tirado.b@icloud.com>
2025-02-07 11:08:18 -0600

    Mi primer archivo de python

---
+++ 00_hello.py
@@ -0,0 +1,32 @@
+# Esto es un comentario
+
+#Para mostrar un mensaje en consola se tiene que utilizar el comando print()
+print("Hola, estoy empezando a utilizar Python")
+
+#Se puede usar comilla doble o comilla simple
+print('Hola, estoy empezando a utilizar Python 2.0')
+
+#Si no esta entre comillas lo que esta en el print marcará error de syntaxis si no es algúna operación
+
+
+"""
+Comentario 
+en 
+varias 
+líneas
+"""
+
+'''
+Comentario entre
+líneas
+'''
+
+#Para mostrar el tipo de dato se utiliza...
+print(type("Hola")) #Este sería un str
+print(type(5)) #int
+print(type(5.2)) #float
+print(type(True)) #bool
+print(type(3+2j)) #complex
+
+#Se puede utilizar el "+" para concatenar cadenas de texto
+print('Hola '+"a"+" todos")
--- hello.py
+++
@@ -1,5 +0,0 @@
-# Esto es un comentario
-
-#Para mostrar un mensaje en consola se tiene que utilizar el comando print()
-print("Hola, estoy empezando a utilizar Python")
-
